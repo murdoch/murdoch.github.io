220c45b4e5aa007fa13b6ce6aa255508b51ae34b
Stephen Murdoch <stephen@murdo.ch>
2024-10-24 03:25:40 +0100

    make it work

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Stephen Murdoch';
-export const SITE_DESCRIPTION = 'Welcome to my internet website!';
+export const SITE_DESCRIPTION = 'I work as a programmer in the travel industry and live in Largs, Scotland. My interests include video games, music, wildlife, sports, and travelling.';
+export const SITE_IMAGE = '/blog-placeholder-1.jpg';
