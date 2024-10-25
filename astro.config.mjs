6379d7afb42f7ad79f29306a472e4a2a0946a837
Stephen Murdoch <stephen@murdo.ch>
2024-10-25 04:45:47 +0100

    add youtube embeds plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
+import embeds from 'astro-embed/integration';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 	site: 'https://murdo.ch',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		embeds(),
+		mdx(),
+		sitemap()
+	],
 	output: 'static'
 });
