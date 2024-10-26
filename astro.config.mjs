091581a57bfa1e1f256a8b80cdf100a163ea43df
Stephen Murdoch <stephen@murdo.ch>
2024-10-27 00:55:34 +0100

    use github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://murdo.ch',
+	site: 'https://murdoch.github.io',
 	integrations: [
 		embeds(),
 		mdx(),
